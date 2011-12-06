e98f870c5ffe04d9f9489da395eca3c94851f68e
Mikuláš Dítě <rullaf@gmail.com>
2011-12-06 19:46:45 +0100

    add javascript compiler, use compiled version in index.html

---
+++ compiler.php
@@ -0,0 +1,29 @@
+<?php
+
+$files = array(
+	"jquery.1.7.1.min.js",
+	"jquery-ui.1.8.16.min.js",
+	"json2.js",
+	"context_menu.js",
+
+	"Card.js",
+	"Game.js",
+	"Player.js",
+	"Board.js",
+	"Container.js",
+	"Hand.js",
+	"Pile.js",
+
+	"main.js",
+);
+
+$file = fopen("compiled.js", "w");
+foreach ($files as $filename) {
+	foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(".")) as $path => $info) {
+		if ($info->getFilename() == $filename) {
+			fwrite($file, "\n" . file_get_contents($path));
+		}
+	}
+}
+
+fclose($file);
